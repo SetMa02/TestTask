68f476d679e3414125259e07d16fe888972000b9
SetMa02 <maxuser99@mail.ru>
2021-04-26 15:27:19 +0700

    Код упростился

--- Program.cs
+++ Program.cs
@@ -27,31 +27,10 @@ namespace TestTask
                     letterCounts = 0;
                     chis = chis.Where(x => x != ch).ToArray();
                 }
-                int i = ToInt(ch);
-               switch(i)
-                {
-                    case 0: counts[0]++; break;
-                    case 1: counts[1]++; break;
-                    case 2: counts[2]++; break;
-                    case 3: counts[3]++; break;
-                    case 4: counts[4]++; break;
-                    case 5: counts[5]++; break;
-                    case 6: counts[6]++; break;
-                    case 7: counts[7]++; break;
-                    case 8: counts[8]++; break;
-                    case 9: counts[9]++; break;
-                }
-            }
-            for(int x = 0; x < counts.Length; x++)
-            {
-                if(counts[x] > 0)
-                    Console.WriteLine(x + ":" + " " + counts[x]);
+            
             }
             Console.ReadKey();
         }
-        static int ToInt(char c)
-        {
-            return (c - '0');
-        }
+
     }
 }
