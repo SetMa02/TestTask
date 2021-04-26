938a735fc56e13c6156b5e850789d63730263508
SetMa02 <maxuser99@mail.ru>
2021-04-26 15:13:50 +0700

    Теперь считает и буквы

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Linq;
 
 namespace TestTask
 {
@@ -10,9 +10,23 @@ namespace TestTask
             Console.Write("Input: ");
             string input = Console.ReadLine();
             char[] chis = input.ToCharArray();
+            int letterCounts = 0;
             int[] counts = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             foreach(var ch in chis)
             {
+                for(int y = 0 ;y < chis.Length; y++)
+                {
+                    if(ch == chis[y])
+                    {
+                        letterCounts++;
+                    }
+                }
+                if(letterCounts != 0 && letterCounts != 1)
+                {
+                    Console.WriteLine(ch + ":" + " " + letterCounts);
+                    letterCounts = 0;
+                    chis = chis.Where(x => x != ch).ToArray();
+                }
                 int i = ToInt(ch);
                switch(i)
                 {
